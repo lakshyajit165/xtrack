4963b766c6b255c133df0541b18da33eca7c0ac6
Lakshyajit <lakshyajit165@gmail.com>
2020-10-08 16:44:31 +0530

    ground work for routing

---
+++ xtrack-app/xtrack-app/src/app/menu/menu.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+import { MenuPage } from './menu.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'menu/home'
+  },
+  {
+    path: 'menu',
+    component: MenuPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: '../home/home.module#HomePageModule'
+      },
+      {
+        path: 'about',
+        loadChildren: '../about/about.module#AboutPageModule'
+      }
+    ]
+  }
+ 
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MenuPage]
+})
+
+export class MenuPageModule { }
---
+++ xtrack-app/xtrack-app/src/app/menu/menu.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  activePath = '';
+
+  pages = [
+   
+    {
+      name: 'Home',
+      path: '/xtrack/menu/home'
+    },
+    {
+      name: 'About',
+      path: '/xtrack/menu/about'
+    }
+  ]
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.activePath = event.url
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
