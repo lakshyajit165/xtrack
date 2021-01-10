37bbbb54da2fa13769af8fca04fbbce21681e32d
Lakshyajit <lakshyajit165@gmail.com>
2021-01-10 16:50:52 +0530

    modified workflow script

--- ssh_to_server.sh
+++ ssh_to_server.sh
@@ -17,7 +17,7 @@ echo $mysql_password
 echo $mysql_db
 
 # ssh and set the environment variables, and finally run the deployment script
-ssh ubuntu@$server "cd xtrack-app \
+ssh -i ubuntu@$server "cd xtrack-app \
     && export MYSQL_ROOT_PASSWORD=$mysql_password \
     && export MYSQL_DATABASE=$mysql_db \
     && git pull origin stage \
