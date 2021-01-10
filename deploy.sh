2cff1c682d0645bf4e14ae1e66377a087ec78ff4
Lakshyajit <lakshyajit165@gmail.com>
2021-01-10 14:32:05 +0530

    preparing to deploy

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+containers_count=$(docker ps -a | grep . -c)
+
+containers_name=$(docker ps -a | awk 'FNR > 1 {print $NF}')
+
+images_count=$(docker images | grep . -c)
+
+image_names=$(docker images | awk '{print $1}')
+
+
+
+
+# running container count > 1 => stop & remove the containers (except db)
+if [[ $containers_count -gt 1 ]]; then
+    for i in "$containers_name"; do
+        for name in $i; do
+            if [[ $name == "xtrack-server" || $name == "xtrackdb" ]];
+            then
+                docker stop $name
+                docker rm $name
+            fi
+        done
+    done
+fi
+
+
+# image count > 1 => remove the images (except the db)
+if [[ $images_count -gt 1 ]]; then
+    for j in "$image_names"; do
+        for image in $j; do
+            if [[ $image == "mysql" ]]; then
+                docker rmi "$image:8"
+            fi
+
+            if [[ $image == "xtrack-server" ]]; then
+                docker rmi "$image:latest"
+            fi
+        done
+    done
+fi
+
+# start containers again
+docker-compose up --build -d
---
+++ ssh_to_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -f
+
+server=${{ secrets.PROD_SERVER }}
+
+mysql_password=${{ secrets.MYSQL_PASSWORD }} 
+mysql_db=${{ secrets.MYSQL_DB }} 
+
+
+echo 'Starting to Deploy...'
+
+# ssh and set the environment variables, and finally run the deployment script
+ssh ubuntu@$server "cd xtrack-app \
+    && export MYSQL_PASSWORD=$mysql_password \
+    && export MYSQL_DB=$mysql_db \
+    && git pull origin stage \
+    && sudo chmod 777 deploy.sh \
+    && ./deploy.sh"
+
+echo 'Deployment completed successfully'
+# ${{ secrets.SSH_KEY }}
