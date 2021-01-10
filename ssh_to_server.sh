0a0cc941434653d7e69f6429b25692db432fd63e
Lakshyajit <lakshyajit165@gmail.com>
2021-01-10 15:03:16 +0530

    adding key to known hosts

--- ssh_to_server.sh
+++ ssh_to_server.sh
@@ -5,7 +5,7 @@ set -f
 server=$PROD_SERVER
 
 mysql_password=$MYSQL_ROOT_PASSWORD 
-mysql_db=$MYSQL_DB 
+mysql_db=$MYSQL_DATABASE 
 
 
 echo 'Starting to Deploy...'
@@ -19,7 +19,7 @@ echo $mysql_db
 # ssh and set the environment variables, and finally run the deployment script
 ssh ubuntu@$server "cd xtrack-app \
     && export MYSQL_ROOT_PASSWORD=$mysql_password \
-    && export MYSQL_DB=$mysql_db \
+    && export MYSQL_DATABASE=$mysql_db \
     && git pull origin stage \
     && sudo chmod 777 deploy.sh \
     && ./deploy.sh"
