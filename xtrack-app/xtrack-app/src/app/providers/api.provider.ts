d6f47f78a6d55097af3aa20810350d1973e5f468
Lakshyajit <lakshyajit165@gmail.com>
2021-02-02 21:17:04 +0530

    minor UI modification in homepage

--- xtrack-app/xtrack-app/src/app/providers/api.provider.ts
+++ xtrack-app/xtrack-app/src/app/providers/api.provider.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class api {
 
-    public serviceRoute: string = 'http://65.1.91.156:5000/api/';
+    public serviceRoute: string = 'http://52.66.240.206:5000/api/';
 
     public constructor() { }
 
